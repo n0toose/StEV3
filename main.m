e3e7c9edcfdcbf6755bb14d3da09f9631a400f7f
Panagiotis Vasilopoulos <hello@alwayslivid.com>
2021-12-21 14:12:05 +0100

    Add EV3 functionality

--- main.m
+++ main.m
@@ -16,15 +16,31 @@ joy = vrjoystick(1);
 % Keep in mind that the shoulders are
 % not supported properly.
 
+addpath('ev3-toolbox-matlab/source/')
+
 figure
+joy = vrjoystick(1);
 hold on
 grid on
 xlim([-1 1])
 ylim([-1 1])
-tic
+
+b = EV3();
+b.connect('usb', 'beep', 'on');
+debug = 'off'
+startedPrevious = false;
+
+b.motorA.stop()
+b.motorB.stop()
+b.motorC.stop()
+b.motorD.stop()
+
+a_running = false;
+b_running = false;
+c_running = false;
+d_running = false;
 
 while true
-    toc
     a_pressed = button(joy, 1);
     b_pressed = button(joy, 2);
     x_pressed = button(joy, 3);
@@ -43,20 +59,94 @@ while true
     left_horizontal_stick = axis(joy, 1);
     left_vertical_stick = -axis(joy, 2);    
     left_shoulder = axis(joy, 6);
+    left_shoulder_button = button(joy, 7);
 
     right_horizontal_stick = axis(joy, 3);
     right_vertical_stick = -axis(joy, 4);
     right_shoulder = axis(joy, 5);
+    right_shoulder_button = button(joy, 8);
 
-    dpad_horizontal = axis(joy, 8);
-    dpad_vertical = axis(joy, 7);
+    dpad_horizontal = axis(joy, 7);
+    dpad_vertical = axis(joy, 8);
 
-    plot(left_horizontal_stick, left_vertical_stick, 'or')
-    plot(right_horizontal_stick, right_vertical_stick, 'ob')
-    pause(0.01);
+    if left_shoulder > -1 || right_shoulder > -1 ...
+        || right_shoulder_button == 1 || left_shoulder_button == 1 ...
+        || dpad_horizontal ~= 0 || dpad_vertical ~= 0
+        if startedPrevious == false;
+            if dpad_horizontal ~= 0
+                startedPrevious = true;
+                power = dpad_horizontal * 50
+                b.motorD.setProperties('debug', debug, 'power', power, 'brakeMode', 'Brake')
+                b.motorD.start()
+            end
 
-    if start_button_pressed == 1
-        close all;
-        break;
+            if dpad_vertical ~= 0
+                startedPrevious = true;
+                a_running = true;
+                b_running = true;
+                power = dpad_vertical * 50
+                b.motorA.setProperties('debug', debug, 'power', power, 'brakeMode', 'Brake')
+                b.motorA.syncedStart(b.motorB)
+            end
+
+            if left_shoulder > -1 && not(a_running) && not(b_running)
+                startedPrevious = true;
+                a_running = true;
+                b_running = true;
+                % power = abs(left_shoulder + 1) * 50
+                power = 100;
+                b.motorA.setProperties('debug', debug, 'power', power, 'brakeMode', 'Coast')
+                b.motorA.syncedStart(b.motorB)
+            elseif right_shoulder > -1 && not(a_running) && not(b_running)
+                startedPrevious = true;
+                a_running = true;
+                b_running = true;
+                % power = abs(right_shoulder + 1) * -50
+                power = -100;
+                b.motorA.setProperties('debug', debug, 'power', power, 'brakeMode', 'Coast')
+                b.motorA.syncedStart(b.motorB)
+            end
+
+            if right_shoulder_button == 1 && not(a_running)
+                startedPrevious = true;
+                power = -100;
+                b.motorA.setProperties('debug', debug, 'power', power, 'brakeMode', 'Brake')
+                b.motorA.start()
+            elseif left_shoulder_button == 1 && not b_running
+                startedPrevious = true;
+                power = -100;
+                b.motorB.setProperties('debug', debug, 'power', power, 'brakeMode', 'Brake')
+                b.motorB.start()
+            end
+        end
+    else
+        startedPrevious = false;
+        a_running = false;
+        b_running = false;
+        c_running = false;
+        d_running = false;
+
+        b.motorA.stop()
+        b.motorB.stop()
+        b.motorC.stop()
+        b.motorD.stop()
+        end
+
+        if b_pressed
+            b.playTone(100, 5000, 5)
+        end
+
+        if a_pressed
+            b.beep()
+        end
+
+        plot(left_horizontal_stick, left_vertical_stick, 'or')
+        plot(right_horizontal_stick, right_vertical_stick, 'ob')
+        pause(0.01);
+
+        if start_button_pressed == 1
+            close all;
+            break;
+        end
     end
 end
