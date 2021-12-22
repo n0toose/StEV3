116c71bbec1d676b3c41cfaf6835fce254185ca6
Panagiotis Vasilopoulos <hello@alwayslivid.com>
2021-12-22 14:46:23 +0100

    Cleanup: Remove unnecessary conditions, add features
    - Suppress output of some variable assignments
    - Improve indentation
    - Add experimental distance sensor feature
    - Use Bluetooth by default to control the vehicle, instead of USB
    - Comment out plotting code, as it causes performance issues with
      the kart itself. (I wish I used multiple threads for this.)

--- main.m
+++ main.m
@@ -1,6 +1,8 @@
 function main
 
 addpath('ev3-toolbox-matlab/source/')
+addpath(pwd)
+global joy
 
 % ----------------------------------
 % Xbox One Controller's Capabilities
@@ -18,7 +20,7 @@ xlim([-1 1])
 ylim([-1 1])
 
 b = EV3();
-b.connect('usb', 'beep', 'on');
+b.connect('bt', 'serPort', '/dev/rfcomm0', 'beep', 'on');
 debug = 'off'
 startedPrevious = false;
 
@@ -30,11 +32,6 @@ b.motorA.stop()
 b.motorB.stop()
 b.motorC.stop()
 b.motorD.stop()
-
-a_running = false;
-b_running = false;
-c_running = false;
-d_running = false;
 
 while true
     a_pressed = button(joy, 1);
@@ -53,7 +50,7 @@ while true
     right_button_pressed = button(joy, 8);
 
     left_horizontal_stick = axis(joy, 1);
-    left_vertical_stick = -axis(joy, 2);    
+    left_vertical_stick = -axis(joy, 2);
     left_shoulder = axis(joy, 6);
     left_shoulder_button = button(joy, 7);
 
@@ -69,76 +66,80 @@ while true
         || right_shoulder_button == 1 || left_shoulder_button == 1 ...
         || dpad_horizontal ~= 0 || dpad_vertical ~= 0
         if startedPrevious == false;
-            if dpad_horizontal ~= 0
+            if dpad_horizontal ~= 0 % && not(b.motorD.isRunning)
                 startedPrevious = true;
-                power = dpad_horizontal * 50
+                power = dpad_horizontal * 50;
                 b.motorD.setProperties('debug', debug, 'power', power, 'brakeMode', 'Brake')
                 b.motorD.start()
             end
 
-            if dpad_vertical ~= 0
+            if dpad_vertical ~= 0 % && not(a.motorA.isRunning) && not(a.motorB.isRunning)
                 startedPrevious = true;
-                a_running = true;
-                b_running = true;
-                power = dpad_vertical * 50
+                power = dpad_vertical * 50;
                 b.motorA.setProperties('debug', debug, 'power', power, 'brakeMode', 'Brake')
                 b.motorA.syncedStart(b.motorB)
             end
 
-            if left_shoulder > -1 && not(a_running) && not(b_running)
+            if right_shoulder_button == 1 && not(b.motorA.isRunning) && not(b.motorC.isRunning)
                 startedPrevious = true;
-                a_running = true;
-                b_running = true;
+                power = -100;
+                b.motorA.setProperties('debug', debug, 'power', power, 'brakeMode', 'Coast')
+                b.motorA.start()
+                % b.motorA.syncedStart(b.motorD)
+            elseif left_shoulder_button == 1 && not(b.motorB.isRunning) && not(b.motorC.isRunning)
+                startedPrevious = true;
+                power = -100;
+                b.motorB.setProperties('debug', debug, 'power', power, 'brakeMode', 'Coast')
+                b.motorB.start()
+                % b.motorB.syncedStart(b.motorD)
+            end
+
+            if left_shoulder > -1 && not(b.motorA.isRunning) && not(b.motorB.isRunning)
+                startedPrevious = true;
                 % power = abs(left_shoulder + 1) * 50
                 power = 100;
                 b.motorA.setProperties('debug', debug, 'power', power, 'brakeMode', 'Coast')
                 b.motorA.syncedStart(b.motorB)
-            elseif right_shoulder > -1 && not(a_running) && not(b_running)
+            elseif right_shoulder > -1 && not(b.motorA.isRunning) && not(b.motorB.isRunning)
                 startedPrevious = true;
-                a_running = true;
-                b_running = true;
                 % power = abs(right_shoulder + 1) * -50
                 power = -100;
                 b.motorA.setProperties('debug', debug, 'power', power, 'brakeMode', 'Coast')
                 b.motorA.syncedStart(b.motorB)
             end
-
-            if right_shoulder_button == 1 && not(a_running)
-                startedPrevious = true;
-                power = -100;
-                b.motorA.setProperties('debug', debug, 'power', power, 'brakeMode', 'Brake')
-                b.motorA.start()
-            elseif left_shoulder_button == 1 && not(b_running)
-                startedPrevious = true;
-                power = -100;
-                b.motorB.setProperties('debug', debug, 'power', power, 'brakeMode', 'Brake')
-                b.motorB.start()
-            end
         end
     else
         startedPrevious = false;
-        a_running = false;
-        b_running = false;
-        c_running = false;
-        d_running = false;
 
         b.motorA.stop()
         b.motorB.stop()
         b.motorC.stop()
         b.motorD.stop()
     end
-        if x_pressed
-
         if b_pressed
-            b.playTone(100, 5000, 5)
+            song = songPlayer();
+            song.gotye_1(b, joy)
         end
 
         if a_pressed
-            b.beep()
+            b.beep();
         end
 
-        plot(left_horizontal_stick, left_vertical_stick, 'or')
-        plot(right_horizontal_stick, right_vertical_stick, 'ob')
+        % Distance sensor
+        if b.sensor < 20
+            b.playTone(100, 4000, 250)
+        end
+
+        % if b_pressed
+        %     b.playTone(100, 5000, 5)
+        % end
+
+        if right_stick_pressed
+            b.playTone(100, 3000, 250)
+        end
+
+        % plot(left_horizontal_stick, left_vertical_stick, 'or')
+        % plot(right_horizontal_stick, right_vertical_stick, 'ob')
         pause(0.01);
 
         if start_button_pressed == 1
